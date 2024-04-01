0d099abae1b40c080cf4d98baecb51eadcf356eb
Uriel Blanco <uriel.blanco@contractors.roche.com>
2024-04-01 20:05:03 +0200

    feat: add shadcn components

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+		<section>
+			<div className='overflow-hidden
+			px-4
+			sm:px-6
+			mt-10
+			sm:flex
+			sm:flex-col
+			gap-4
+			md:justify-center
+			md:items-center'>
+
+			</div>
+		</section>
+		)
+};
+
+export default HomePage;
